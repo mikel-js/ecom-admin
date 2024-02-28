b7fc6474090b119f8ff7c766cdca9bca4ecec95d
mikel-js <michaelcastro.js@gmail.com>
2024-02-28 21:06:30 +0200

    addproducts

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,5 @@
-import Nav from '@/components/Nav';
-import { useSession, signIn, signOut } from 'next-auth/react';
+import Layout from '@/components/Layout';
 
 export default function Home() {
-  const { data: session } = useSession();
-
-  if (!session) {
-    return (
-      <div className='bg-blue-900 w-screen h-screen flex items-center'>
-        <div className='text-center w-full'>
-          <button
-            className='bg-white p-2 px-4 rounded-lg'
-            onClick={() => signIn('google')}
-          >
-            Login with Google
-          </button>
-        </div>
-      </div>
-    );
-  }
-  return (
-    <div className='bg-blue-900 min-h-screen flex'>
-      <Nav />
-      <div className='bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4'>
-        Signed in as {session.user.email}
-      </div>
-      ;
-    </div>
-  );
+  return <Layout>test</Layout>;
 }
